693a881330de45f37485fb4376d8e7e79e5bd889
Aditya Eka Pranata <aepranata@gmail.com>
2025-01-07 01:09:41 +0700

    config: `Bootanimation` not as package anymore
    Change-Id: Ibceb4a6aa113ab56610689934ab285e1773ab0fa
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -99,12 +99,6 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# Bootanimation
-TARGET_SCREEN_WIDTH ?= 1080
-TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
-
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/curl \
     system/bin/getcap \
