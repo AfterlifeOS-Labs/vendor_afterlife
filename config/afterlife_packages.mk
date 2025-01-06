675d554c3d5e1b42bf763d57118121eb89bb48a4
Aditya Eka Pranata <aepranata@gmail.com>
2025-01-07 01:06:17 +0700

    config: Say goodbye to `AudioFX`
    Change-Id: I281938280196d9a94fa417adf53d4a10c802d798
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- config/afterlife_packages.mk
+++ config/afterlife_packages.mk
@@ -37,11 +37,6 @@ PRODUCT_PACKAGES += \
     Aperture
 endif
 
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
-endif
-
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
