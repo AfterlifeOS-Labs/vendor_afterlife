17e6bed8b95a194d1ede63ddc0a190f7a9d110f1
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-24 22:53:14 +0100

    config: Conditionally build ColumbusService
    Change-Id: I535e1b5f813922acbb9c0b39e0cc63ccb1f19ebc
    [DerpFest: Build it by default]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -61,6 +61,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/afterlife/prebuilt/common/etc/init/init.afterlife-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.afterlife-system_ext.rc
 
+# ColumbusService
+ifneq ($(TARGET_SUPPORTS_QUICK_TAP),false)
+PRODUCT_PACKAGES += \
+    ColumbusService
+endif
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
