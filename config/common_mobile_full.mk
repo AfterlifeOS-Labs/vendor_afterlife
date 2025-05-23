Project-Flare/vendor_flare

config: Move LatinIME stuff to common_mobile*.mk
Change-Id: I5697e1cef84972a6a7f76b8c065e575e1ba17ef8
Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

config/common_mobile_full.mk
@@ -12,3 +12,7 @@ endif
 PRODUCT_PACKAGES += \
     unrar \
     zstd
+
+# Include Flare LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/flare/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/flare/overlay/dictionaries

AfterlifeOS-Labs/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/common_mobile_full.mk
@@ -12,3 +12,7 @@ endif
 PRODUCT_PACKAGES += \
     unrar \
     zstd
+
+# Include Afterlife LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/afterlife/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/afterlife/overlay/dictionaries