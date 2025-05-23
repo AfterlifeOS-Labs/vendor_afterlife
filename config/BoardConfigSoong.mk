Project-Flare/vendor_flare

soong: Add camera package name
Use TARGET_CAMERA_PACKAGE_NAME to specify
the package name.

Suggested-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
Change-Id: I9768d0ac40517b6555fd0606b299760f5baed71b
Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

config/BoardConfigSoong.mk
@@ -43,7 +43,8 @@ SOONG_CONFIG_flareGlobalVars += \
     target_trust_usb_control_disable \
     uses_legacy_fd_fbdev \
     uses_oplus_camera \
-    needs_camera_boottime
+    needs_camera_boottime \
+    target_camera_package_name
 
 # Soong bool variables
 SOONG_CONFIG_flareGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -76,6 +77,7 @@ SOONG_CONFIG_flareGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBD
 SOONG_CONFIG_flareGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_flareGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_flareGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_flareGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)

AfterlifeOS-Labs/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/BoardConfigSoong.mk
@@ -43,7 +43,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     target_trust_usb_control_disable \
     uses_legacy_fd_fbdev \
     uses_oplus_camera \
-    needs_camera_boottime
+    needs_camera_boottime \
+    target_camera_package_name
 
 # Soong bool variables
 SOONG_CONFIG_afterlifeGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -76,6 +77,7 @@ SOONG_CONFIG_afterlifeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD
 SOONG_CONFIG_afterlifeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_afterlifeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_afterlifeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)