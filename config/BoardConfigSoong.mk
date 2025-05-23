Project-Flare/vendor_flare

soong: Add TARGET_CAMERA_BOOTTIME_TIMESTAMP conditional
Change-Id: Ic48acda208fc9990fc5c9159ad06518d48f21477

Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

config/BoardConfigSoong.mk
@@ -42,7 +42,8 @@ SOONG_CONFIG_flareGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_legacy_fd_fbdev \
-    uses_oplus_camera
+    uses_oplus_camera \
+    needs_camera_boottime
 
 # Soong bool variables
 SOONG_CONFIG_flareGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -74,6 +75,7 @@ SOONG_CONFIG_flareGlobalVars_disable_bluetooth_le_set_host_feature := $(TARGET_D
 SOONG_CONFIG_flareGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 SOONG_CONFIG_flareGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_flareGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
+SOONG_CONFIG_flareGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)

AfterlifeOS-Labs/vendor_afterlife

Change-Id: I46712d9f9f37257e83d26734ecd608b601087a12
Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/BoardConfigSoong.mk
@@ -42,7 +42,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_legacy_fd_fbdev \
-    uses_oplus_camera
+    uses_oplus_camera \
+    needs_camera_boottime
 
 # Soong bool variables
 SOONG_CONFIG_afterlifeGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -74,6 +75,7 @@ SOONG_CONFIG_afterlifeGlobalVars_disable_bluetooth_le_set_host_feature := $(TARG
 SOONG_CONFIG_afterlifeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 SOONG_CONFIG_afterlifeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
+SOONG_CONFIG_afterlifeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)