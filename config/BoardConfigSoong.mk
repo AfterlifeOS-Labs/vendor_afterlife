Project-Flare/vendor_flare

soong: Add support for miui camera mode [2/2]
Change-Id: I0c912a1c7056bb204a24bd5f0a08308cae71f3f3
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

config/BoardConfigSoong.mk
@@ -46,7 +46,8 @@ SOONG_CONFIG_flareGlobalVars += \
     needs_camera_boottime \
     target_camera_package_name \
     camera_needs_client_info \
-    uses_nothing_camera
+    uses_nothing_camera \
+    uses_miui_camera
 
 # Soong bool variables
 SOONG_CONFIG_flareGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -83,6 +84,7 @@ SOONG_CONFIG_flareGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKA
 SOONG_CONFIG_flareGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_flareGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_flareGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
+SOONG_CONFIG_flareGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)

AfterlifeOS-Labs/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/BoardConfigSoong.mk
@@ -46,7 +46,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     needs_camera_boottime \
     target_camera_package_name \
     camera_needs_client_info \
-    uses_nothing_camera
+    uses_nothing_camera \
+    uses_miui_camera
 
 # Soong bool variables
 SOONG_CONFIG_afterlifeGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -83,6 +84,7 @@ SOONG_CONFIG_afterlifeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_P
 SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_afterlifeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
+SOONG_CONFIG_afterlifeGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)