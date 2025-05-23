Project-Flare/vendor_flare

soong: Add TARGET_USES_NOTHING_CAMERA
Change-Id: Ib03ed29f9649750fa0f10f28615c109ded21b55a
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
Signed-off-by: aswin7469 <aswinas@pixysos.com>
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

config/BoardConfigSoong.mk
@@ -45,7 +45,8 @@ SOONG_CONFIG_flareGlobalVars += \
     uses_oplus_camera \
     needs_camera_boottime \
     target_camera_package_name \
-    camera_needs_client_info
+    camera_needs_client_info \
+    uses_nothing_camera
 
 # Soong bool variables
 SOONG_CONFIG_flareGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -80,6 +81,7 @@ SOONG_CONFIG_flareGlobalVars_sdmcore_has_is_display_hw_available_func := $(TARGE
 SOONG_CONFIG_flareGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_flareGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_flareGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_flareGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)

AfterlifeOS-Labs/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/BoardConfigSoong.mk
@@ -45,7 +45,8 @@ SOONG_CONFIG_afterlifeGlobalVars += \
     uses_oplus_camera \
     needs_camera_boottime \
     target_camera_package_name \
-    camera_needs_client_info
+    camera_needs_client_info \
+    uses_nothing_camera
 
 # Soong bool variables
 SOONG_CONFIG_afterlifeGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
@@ -80,6 +81,7 @@ SOONG_CONFIG_afterlifeGlobalVars_sdmcore_has_is_display_hw_available_func := $(T
 SOONG_CONFIG_afterlifeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_afterlifeGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
+SOONG_CONFIG_afterlifeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Lineage Health HAL
 ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)