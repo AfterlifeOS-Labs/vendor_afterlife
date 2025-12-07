fdc060c136f217afa16378a12e3cb72ddc53bf5e
hridaya <hridaya@pixelos.net>
2025-12-07 09:50:52 +0545

    config: Drop SimpleSettingsConfig
    Change-Id: I2235ad147a3c36b4e399c61a2adf1909b428a944

--- config/common.mk
+++ config/common.mk
@@ -143,10 +143,6 @@ TARGET_SCREEN_HEIGHT ?= 1920
 PRODUCT_PACKAGES += \
     framework_compatibility_matrix.lineage.xml
 
-# Config
-PRODUCT_PACKAGES += \
-    SimpleSettingsConfig
-
 # Extra tools
 PRODUCT_PACKAGES += \
     bash \
